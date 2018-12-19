8246f187e2e64372a829aaba4e04628a990cbe16
Daniel Shimon <danielzshimon@gmail.com>
2018-12-19 11:48:45 -0500

    more setup

---
+++ syfe/src/components/memory.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Memory extends Component {
+  render() {
+    return (
+      <div className="Memory">
+        
+      </div>
+    );
+  }
+}
+
+export default Memory;
---
+++ syfe/src/components/timeline.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Timeline extends Component {
+  render() {
+    return (
+      <div className="Timeline">
+        
+      </div>
+    );
+  }
+}
+
+export default Timeline;
