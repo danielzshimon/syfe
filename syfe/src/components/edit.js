a5c07a90647fdde846306788c32b29baa4be6873
Daniel Shimon <danielzshimon@gmail.com>
2018-12-19 11:47:34 -0500

    setup

---
+++ syfe/src/components/edit.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Edit extends Component {
+  render() {
+    return (
+      <div >
+        
+          
+        
+      </div>
+    );
+  }
+}
+
+export default Edit;
